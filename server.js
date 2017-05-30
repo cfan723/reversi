d0f7446f09a6907088a4c69fdb087825914cf46b
Charlene Fan <charlenefan@Charlenes-MacBook-Pro.local>
2017-05-29 19:43:55 -0700

    changes

--- server.js
+++ server.js
@@ -59,8 +59,6 @@ io.sockets.on('connection', function(socket){
 		socket.broadcast.emit('log',array);
 	}
 	
-	log('A web site connected to the server');
-	
 	
 	/*join_room command */
 	/* payload:
@@ -73,7 +71,7 @@ io.sockets.on('connection', function(socket){
 				'result': 'success',
 				'room' : room joined,
 				'username' : username that joined,
-				'socket_id': the socket id of the person that,
+				'socket_id': the socket id of the person that joined,
 				'membership': number of people in the room including the new one
 			}
 			or
@@ -84,6 +82,9 @@ io.sockets.on('connection', function(socket){
 
 	socket.on('join_room',function(payload){
 		log('\'join_room\' command' + JSON.stringify(payload));
+
+		/*check that the client sent a payload */
+
 		if('undefined' === typeof payload || !payload) {
 		var error_message = 'join_room had no payload, command aborted';
 		log(error_message);
@@ -93,6 +94,8 @@ io.sockets.on('connection', function(socket){
 										});
 		return;
 		}
+
+		/* check that the payload has a room to join */
 
 		var room = payload.room;
 		if('undefined' === typeof room || !room) {
@@ -142,7 +145,7 @@ io.sockets.on('connection', function(socket){
 								socket_id: socket.id,
 								membership: numClients
 							};
-		io.in(room).emit('join_room_response',success_data);
+		io.in(room).emit('join_room_response', success_data);
 
 		for (var socket_in_room in roomObject.sockets) {
 			var success_data = {
@@ -154,6 +157,7 @@ io.sockets.on('connection', function(socket){
 							};
 			socket.emit('join_room_response ', success_data);
 		}
+
 		log('join_room success');
 
 		});
@@ -162,9 +166,10 @@ io.sockets.on('connection', function(socket){
 		log('Client disconnected '+JSON.stringify(players[socket.id]));
 
 		if('undefined' !== typeof players[socket.id] && players[socket.id]) {
-			var username = players[socket.id].room;
+			var username = players[socket.id].username;
+			var room = players[socket.id].room;
 			var payload = {
-							username:username,
+							username: username,
 							socket_id: socket.id
 			};
 			delete players[socket.id];
